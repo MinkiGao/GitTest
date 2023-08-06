e2d9605b8f42414abb53f2da633c647f552a1cbb
south <100817841+southwindg@users.noreply.github.com>
2023-08-06 13:34:36 +0800

    Create Students.py

---
+++ Students.py
@@ -0,0 +1,10 @@
+class Students():
+
+    def __init__(self, name, num):
+        self.name = name
+        self.num = num
+    
+    def print_info(self):
+        print("name:", self.name)
+        print("school num:",self.num)
+
