fa3970f5de643faa03ceeb24e68ff0b4cf75d26e
Mengqi <2534671415@qq.com>
2023-08-26 14:47:31 +0800

    feat: add a file

---
+++ Fruit.py
@@ -0,0 +1,8 @@
+class Fruit():
+    def __init__(self, categorty, color):
+        self.categorty = categorty
+        self.color = color
+    
+    def print_fruit_info(self):
+        print('categorty',self.categorty)
+        print('color', self.color)
